176c7218214b81d603d0d046f2b65427be27cc50
ica574 <iciliaat@gmail.com>
2022-10-08 23:25:57 +0200

    Added main file that utilises the QFT circuit.

---
+++ main.py
@@ -0,0 +1,31 @@
+# Name: main.py
+# Author: Isaac Cilia Attard
+# Date: 08/10/2022
+# Description: Offers an image to the QFT circuit and displays the results.
+
+"""Qiskit packages"""
+from qiskit import Aer
+from qiskit.visualization import plot_bloch_multivector
+"""Matplotlib package"""
+import matplotlib.pyplot as plt
+"""Circuit and encoder packages"""
+from circuit import qft
+from encoder import encode_image, image_to_binary
+
+circuit = encode_image(image_to_binary("test.png")) # Encodes an image onto a quantum circuit
+simulator = Aer.get_backend("aer_simulator") # Qiskit Aer simulator instantiation
+
+"""Computational basis qubits"""
+qc_init = circuit.copy()
+qc_init.save_statevector()
+statevector = simulator.run(qc_init).result().get_statevector()
+plot_bloch_multivector(statevector)
+
+"""Fourier basis qubits"""
+qft(circuit, image_to_binary("test.png").size) # Applies QFT to encoded image
+circuit.save_statevector()
+statevector = simulator.run(circuit).result().get_statevector()
+plot_bloch_multivector(statevector)
+
+circuit.draw(output='mpl') # Renders circuit before showing it
+plt.show()
